45dbffda69bdfb1a108aa085032231b4549e827b
Ben Davis <bendavis493@gmail.com>
2024-09-26 11:46:21 -0600

    added error handling

--- connection.js
+++ connection.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cse341.xe5s3.mongodb.net/ExampleData?retryWrites=true&w=majority&appName=CSE341`;
 
 async function connectDB() {
-    await mongoose.connect(uri)
+    try {
+        await mongoose.connect(uri)
+    }
+    catch (err) {
+        console.error('connection to database failed: ' + err);
+    }
 }
 module.exports = connectDB;
