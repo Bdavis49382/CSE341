b59f6adb92d92c8786f4f89a1705d9f3366534ac
Ben Davis <bendavis493@gmail.com>
2024-10-11 11:29:01 -0600

    added a schema

--- swagger.js
+++ swagger.js
@@ -5,6 +5,29 @@ const doc = {
     title: "Contacts API",
     description: "An API which keeps track of contacts.",
   },
+  components: {
+    schemas: {
+      contact: {
+        $firstName: "string",
+        $lastName: "string",
+        $email: "string",
+        $favoriteColor: "string",
+        $birthday: "string"
+      }
+    },
+    examples: {
+      basicExample: {
+        value: {
+          "first_name": "Rudolph",
+          "last_name": "Reindeer",
+          "email": "tester@fake.com",
+          "favorite_color": "red",
+          "birthday": "Sept 2 2000"
+        },
+        summary: "Sample"
+    }
+  }
+},
   host: "cse341-o31n.onrender.com",
 };
 
